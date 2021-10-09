048e0232d9da7b73065d22df2cfa09d5c6c3f82e
progbits <53880655+progbits@users.noreply.github.com>
2021-10-09 20:39:05 +0100

    Move AST printing functions into printer.ml

--- spacebar.ml
+++ spacebar.ml
@@ -2,5 +2,5 @@ let _ =
   try
     let lexbuf = Lexing.from_channel stdin in
     let program = Parser.translation_unit Lexer.token lexbuf in
-    Ast.print_translation_unit program
+    Printer.print_translation_unit program
   with End_of_file -> exit 0
